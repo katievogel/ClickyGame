371ac21af0723560d25f52c6407889c6d6dbc605
Katie Vogel <kvog983@gmail.com>
2019-09-05 20:37:05 -0400

    populated images with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import FirstPage from './components/FirstPage';
 import './App.css';
+
+var images = ['Titan.jpg', 'titan_chubby.jpg', "titan_hide_nose.jpg", "titan_jedi.jpg", "titan_kitten.jpg", "titan_post_turkey.jpg", "titan_tucked_in.jpg", "titan_windowsill.jpg"];
+
+function Card (props) {
+  return (
+    <div className="">
+      <img src={props.image}/>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <ul>
+          <li className="brand">
+            <a href="/">Clicky Game</a>
+          </li>
+          <li className="">You guessed incorrectly!</li>
+          <li>Score: 0 | Top Score: 3</li>
+        </ul>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Clicky Game!</h1>
+        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
       </header>
-    </div>
+      {images.map(images => (
+        <Card image={images}/>
+      ))}
+      </div>
   );
 }
+
 
 export default App;

cbb8ce544c5a5541f3cb7809fd04654def4110a0
Katie Vogel <kvog983@gmail.com>
2019-09-08 17:20:00 -0400

    score, highScore, shuffle works

--- src/App.js
+++ src/App.js
@@ -1,38 +1,90 @@
-import React from 'react';
-import FirstPage from './components/FirstPage';
+import React, { Component } from 'react';
+import Card from './components/FirstPage';
 import './App.css';
+import titan from './Titan.json';
 
-var images = ['Titan.jpg', 'titan_chubby.jpg', "titan_hide_nose.jpg", "titan_jedi.jpg", "titan_kitten.jpg", "titan_post_turkey.jpg", "titan_tucked_in.jpg", "titan_windowsill.jpg"];
+function shuffleTiles(titanArray) {
+  for (let i = titanArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const shift = titanArray[i];
+    titanArray[i] = titanArray[j];
+    titanArray[j] = shift;
+  }
+  return titanArray;
+};
 
-function Card (props) {
-  return (
-    <div className="">
-      <img src={props.image}/>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    titan,
+    score: 0,
+    highScore: 0
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <nav className="navbar">
-        <ul>
-          <li className="brand">
-            <a href="/">Clicky Game</a>
-          </li>
-          <li className="">You guessed incorrectly!</li>
-          <li>Score: 0 | Top Score: 3</li>
-        </ul>
-      </nav>
-      <header className="App-header">
-        <h1>Clicky Game!</h1>
-        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
-      </header>
-      {images.map(images => (
-        <Card image={images}/>
-      ))}
+  highScoreCheck() {
+    let highScore = this.state.highScore;
+    if (this.state.score >= highScore) {
+      highScore = this.state.score;
+    }
+    else {
+      highScore = this.state.highScore;
+    }
+    return highScore;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li className="brand">
+              <a href="/">Clicky Game</a>
+            </li>
+            <li className="">You guessed incorrectly!</li>
+            <li>Score: {this.state.score} | Top Score: {this.state.highScore}</li>
+          </ul>
+        </nav>
+        <header className="App-header">
+          <h1>Clicky Game!</h1>
+          <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+        </header>
+        {/* <button onClick={() => this.setState({ counter: this.state.counter + 1 })}></button> */}
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        {this.state.titan.map((titan, i) => (
+          <Card
+            key={titan.id}
+            image={titan.image}
+            onCardClick={() => {
+              if (this.state.titan[i].clicked === false) {
+                console.log("card click from App " + titan.image)
+                const newTitan = [...this.state.titan];
+                newTitan[i].clicked = true;
+                this.setState({
+                  score: this.state.score + 1,
+                  titan: shuffleTiles(newTitan)
+                });
+              } else {
+                console.log("U haz lost.")
+
+                const resetTitan = [...this.state.titan].map(t => {
+                  let t2 = { ...t };
+                  t2.clicked = false;
+                  return t2;
+                })
+
+                //let switchedTiles = shuffleTiles(titan);
+                this.setState({
+                  highScore: this.highScoreCheck(),
+                  score: 0,
+                  titan: shuffleTiles(resetTitan)
+                });
+                //this.setState({ titan: switchedTiles });
+              }
+            }}
+          />
+        ))}
       </div>
-  );
+    );
+  }
 }
 
 
--- src/components/FirstPage/index.js
+++ src/components/FirstPage/index.js
@@ -1,2 +1,12 @@
 import React from "react";
 import "./styles.css";
+
+function Card(props) {
+    return (
+      <div className="tiles" onClick={props.onCardClick}>
+        <img src={props.image} alt="cat pic" />
+      </div>
+    );
+  }
+
+  export default Card;

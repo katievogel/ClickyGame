2af1fb6d477b2b10278a90c39fa26487c4f2468f
Katie Vogel <kvog983@gmail.com>
2019-09-09 19:57:04 -0400

    edited gitignore

---
+++ build/precache-manifest.f166863ab723c2eeb7b26424d7b8f72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b900aefd772f48c91b94782c62b0ad9",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "27c695efe3804d65e5e6",
+    "url": "/ClickyGame/static/css/main.64597e94.chunk.css"
+  },
+  {
+    "revision": "6e7fced5d71044e9d365",
+    "url": "/ClickyGame/static/js/2.47aa9010.chunk.js"
+  },
+  {
+    "revision": "27c695efe3804d65e5e6",
+    "url": "/ClickyGame/static/js/main.3dedfd91.chunk.js"
+  },
+  {
+    "revision": "55999473163dc9306e78",
+    "url": "/ClickyGame/static/js/runtime~main.06dd508e.js"
+  }
+]);

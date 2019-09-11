5b78ced1c2047f9d68238fcac2b5a462d5d8eb75
Katie Vogel <kvog983@gmail.com>
2019-09-10 23:03:40 -0400

    Updates

--- precache-manifest.e58249461a950ca86db17e06a84dd64a.js
+++ precache-manifest.e58249461a950ca86db17e06a84dd64a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "271b4e541694cda1f7df90f99fc45b2d",
+    "revision": "21db44a7a3c3c2cc84ad0a671637bcf0",
     "url": "/ClickyGame/index.html"
   },
   {
-    "revision": "a3ff1b43d56528e23db2",
+    "revision": "7f0f7361ca0b9329ff77",
     "url": "/ClickyGame/static/css/main.64597e94.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ClickyGame/static/js/2.47aa9010.chunk.js"
   },
   {
-    "revision": "a3ff1b43d56528e23db2",
-    "url": "/ClickyGame/static/js/main.363693ab.chunk.js"
+    "revision": "7f0f7361ca0b9329ff77",
+    "url": "/ClickyGame/static/js/main.cdc97bac.chunk.js"
   },
   {
     "revision": "55999473163dc9306e78",
